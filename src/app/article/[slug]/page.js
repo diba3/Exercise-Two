7a05ab7c2c42739cc2504c30608d46201916d770
Diba Chowdhury <dibachowdhury@10-18-156-246.dynapool.wireless.nyu.edu>
2024-09-19 08:09:37 -0400

    09/19/24

---
+++ src/app/article/[slug]/page.js
@@ -0,0 +1,11 @@
+// import styles from './page.module.css";
+
+export default function Article({ params }) {
+    return (
+      <main>
+        <h1>Article: {params.slug}</h1>
+      </main>
+  
+    );
+  }
+  
