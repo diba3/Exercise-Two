82965a30d59781f0f75ea0cb589acb26a30ffa93
Diba Chowdhury <dibachowdhury@10-18-156-246.dynapool.wireless.nyu.edu>
2024-09-19 08:21:09 -0400

    09/19/24

---
+++ src/app/components/articleCard.js
@@ -0,0 +1,8 @@
+export default function ArticleCard() {
+
+    return (
+        <div>
+            <h2>aritcle</h2>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 // import styles from './page.module.css";
 
 import data from "./data.json";
+import ArticleCard from "./components/articleCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Article Listing</h1>
-      {data.map((article) => (
-        <h2>{article.title}</h2>
+      {data.map((article, i) => (
+        <ArticleCard />
       ))}
     </main>
 
